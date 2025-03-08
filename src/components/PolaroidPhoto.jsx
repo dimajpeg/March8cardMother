6d0ee666e256fb88da803db40e51305b45fb9abc
dima <>
2025-03-08 09:29:04 +0200

    Обновления проекта

---
+++ src/components/PolaroidPhoto.jsx
@@ -0,0 +1,13 @@
+// src/components/PolaroidPhoto.jsx
+import React from 'react';
+import './PolaroidPhoto.css';  // Импортируем стиль для фото
+
+const PolaroidPhoto = () => {
+    return (
+        <div className="polaroid">
+            <img src="/rimma5/pal1.jpg" alt="Фото с ней" className="polaroid-img" />
+        </div>
+    );
+};
+
+export default PolaroidPhoto;
